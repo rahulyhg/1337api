ea113081ca52da0f8385ef0a9b96d39a2c1b6b43
Elijah Vs Jesus <elias.hatem@gmail.com>
2015-07-31 02:29:38 -0300

    add favicon.

--- www/admin/index.php
+++ www/admin/index.php
@@ -11,6 +11,7 @@
 	<meta charset='utf-8'>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/gif" href="assets/favicon.gif"/>
 
 	<title>Admin Dashboard</title>
 	<meta name="description" content="">
