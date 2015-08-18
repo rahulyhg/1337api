1a61cdfb3e036a8f81e82b4e46a1b4f2fd86ab52
Elijah Vs Jesus <elias.hatem@gmail.com>
2015-08-18 00:41:01 -0300

    minor responsive changes.

--- www/admin/index.php
+++ www/admin/index.php
@@ -34,28 +34,24 @@
 		<!-- NAVIGATION - HEADER MENU -->
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-				<span class="sr-only">Toggle navigation</span>
+				<span class="sr-only">Menu Principal</span>
 				<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="/admin">umstudio.com</a>
+			<a class="navbar-brand" href="#">umstudio.com</a>
 		</div>
 		<!-- END NAVIGATION - HEADER MENU -->
 
 		<!-- NAVIGATION - TOP MENU -->
 		<ul class="nav navbar-right top-nav">
-			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Elijah Hatem <b class="caret"></b></a>
-				<ul class="dropdown-menu">
-					<li>
-						<a href="#"><i class="fa fa-fw fa-user"></i> Minha Conta</a>
-					</li>
-					<li>
-						<a href="#"><i class="fa fa-fw fa-gear"></i> Configurações</a>
-					</li>
+			<li dropdown keyboard-nav>
+				<a class="dropdown-toggle" dropdown-toggle>
+					<i class="fa fa-user"></i> Elijah Hatem <b class="caret"></b>
+				</a>
+				<ul class="dropdown-menu" role="menu" aria-labelledby="simple-btn-keyboard-nav">
+					<li role="menuitem"><a href="#/"><i class="fa fa-fw fa-user"></i> Minha Conta</a></li>
+					<li role="menuitem"><a href="#/"><i class="fa fa-fw fa-gear"></i> Configurações</a></li>
 					<li class="divider"></li>
-					<li>
-						<a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-					</li>
+					<li role="menuitem"><a href="#/"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
 				</ul>
 			</li>
 		</ul>
