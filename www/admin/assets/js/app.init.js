c8eda4312d95f5b32e8414c1a08df1214be54fed
Elijah Hatem <elias.hatem@gmail.com>
2015-08-18 10:20:00 -0300

    Organize code.

--- www/admin/assets/js/app.init.js
+++ www/admin/assets/js/app.init.js
@@ -1,10 +1,10 @@
 /* ************************************************************
-INIT
+ANGULAR INIT
 ************************************************************ */
 var AdminApp = angular.module('AdminApp', ['ngRoute', 'angular-json-editor', 'ui.bootstrap']);
 
 /* ************************************************************
-ANGULAR MODULES CONFIG
+ANGULAR ROUTES
 ************************************************************ */			
 
 // NG Routes Provider
@@ -100,72 +100,91 @@ AdminApp.config([
 	}
 ]);
 
+/* ************************************************************
+ANGULAR JSON EDITOR
+************************************************************ */			
+
+// GLOBAL UPLOAD AJAX FUNCTION
+// **************************** */			
+
+var jeUploadFunction = function(type, file, cbs) {
+							
+						if (file) {
+							var reader = new FileReader();
+
+							reader.onloadend = function(evt){
+								var b = evt.target.result;
+								var uploadData = '{"filename":"'+file.name+'", "filesize":"'+file.size+'", "blob":"'+b+'"}';
+								var percentComplete = 0;
+
+									$.ajax({
+										xhr: function () {
+											var xhr = new window.XMLHttpRequest();
+											xhr.upload.addEventListener("progress", function (evt) {
+												if (evt.lengthComputable) {
+													var percentComplete = Math.round((evt.loaded / evt.total)*100);
+													if(percentComplete <=100) {
+														cbs.updateProgress(percentComplete);
+													}
+												}
+											}, false);
+											return xhr;
+										},
+										type: 'POST',
+										// TODO: need to pass "edge" at upload url.
+										url: 'api/upload/page',
+										contentType: "application/json; charset=utf-8",
+										dataType: "json",
+										data: uploadData,
+									
+										success: function( data, textStatus, jQxhr ){
+											cbs.success(''+data.id+'');
+										},
+										error: function( jqXhr, textStatus, errorThrown ){
+											console.log( errorThrown );
+										}
+									});
+
+							};
+							reader.readAsDataURL(file);
+						};
+					};
+
+// GLOBAL JSON EDITOR CONFIG
+// **************************** */			
+
+var jePluginsConfig = {
+						sceditor: {
+							style: 			'assets/css/libs/jquery.sceditor.default.min.css',
+							toolbar: 		'bold,italic,underline|strike,subscript,superscript|link,unlink|removeformat|bulletlist,orderedlist|source',
+							locale: 		'pt-BR',
+							emoticonsEnabled: 	false,
+							width: 				'98%',
+							resizeEnabled: 		false,
+						}
+					};
+
+var jeOptionsConfig = {
+						iconlib: 			'fontawesome4',
+						theme: 				'bootstrap3',
+						ajax: 				true,
+						disable_collapse: 	true,
+						disable_edit_json: 	true,
+						disable_properties: true,
+						upload: 			jeUploadFunction,
+					};
+
 // JSON Editor Provider
+// **************************** */			
+
 AdminApp.config( function(JSONEditorProvider) {
 	JSONEditorProvider.configure({
-		plugins: {
-			sceditor: {
-				plugins: 			'',
-				style: 				'assets/css/libs/jquery.sceditor.default.min.css',
-				toolbar: 			'bold,italic,underline|strike,subscript,superscript|link,unlink|removeformat|bulletlist,orderedlist|source',
-				locale: 			'pt-BR',
-				emoticonsEnabled: 	false,
-				width: 				'98%',
-				resizeEnabled: 		false,
-			}
-		},
-		defaults: {
-			options: {
-				iconlib: 			'fontawesome4',
-				theme: 				'bootstrap3',
-				ajax: 				true,
-				disable_collapse: 	true,
-				disable_edit_json: 	true,
-				disable_properties: true,
-				upload: 			function(type, file, cbs) 
-									{
-										if (file) {
-											var reader = new FileReader();
-
-											reader.onloadend = function(evt){
-												var b = evt.target.result;
-												var uploadData = '{"filename":"'+file.name+'", "filesize":"'+file.size+'", "blob":"'+b+'"}';
-												var percentComplete = 0;
-
-													$.ajax({
-														xhr: function () {
-															var xhr = new window.XMLHttpRequest();
-															xhr.upload.addEventListener("progress", function (evt) {
-																if (evt.lengthComputable) {
-																	var percentComplete = Math.round((evt.loaded / evt.total)*100);
-																	if(percentComplete <=100) {
-																		cbs.updateProgress(percentComplete);
-																	}
-																}
-															}, false);
-															return xhr;
-														},
-														type: 'POST',
-														url: 'api/upload/page',
-														contentType: "application/json; charset=utf-8",
-														dataType: "json",
-														data: uploadData,
-													
-														success: function( data, textStatus, jQxhr ){
-															cbs.success(''+data.id+'');
-														},
-														error: function( jqXhr, textStatus, errorThrown ){
-															console.log( errorThrown );
-														}
-
-													});
-
-											};
-											reader.readAsDataURL(file);
-										};
-									}
-
-				}
-			}
+		plugins: jePluginsConfig,
+		defaults: { options: jeOptionsConfig }
 	});
 });
+
+/* ************************************************************
+NPROGRESS CONFIG
+************************************************************ */	
+NProgress.configure({ parent: '#page-wrapper' });
