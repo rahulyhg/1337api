b1995bf7c6625e16972166ef2ec17878a455cc7e
Elijah Vs Jesus <elias.hatem@gmail.com>
2015-09-09 01:59:13 -0300

    gjslint.

--- www/admin/assets/js/app.controllers.js
+++ www/admin/assets/js/app.controllers.js
@@ -3,270 +3,269 @@ ANGULAR ADMIN APP CONTROLLERS
 ************************************************************ */
 
 // Main Controller
-AdminApp.controller('MainController', function ($rootScope, $scope, $location, $localStorage, authService, apiService) {
+AdminApp.controller('MainController', function($rootScope, $scope, $location, $localStorage, authService, apiService) {
 
-	$scope.$on("$routeChangeStart", function() {
-		// fired on success of viewContent load.
-	});
+    $scope.$on('$routeChangeStart', function() {
+        // fired on success of viewContent load.
+    });
 
-	$scope.$on("$routeChangeSuccess", function() {
-		// fired on success of routeChange.
-	});
+    $scope.$on('$routeChangeSuccess', function() {
+        // fired on success of routeChange.
+    });
 
-	$scope.$on('$viewContentLoaded', function(){
-		// fired on success of viewContent load.
-	});
+    $scope.$on('$viewContentLoaded', function() {
+        // fired on success of viewContent load.
+    });
 
-	function successAuth(res) {
-		$localStorage.token = res.token;
-			console.log('authentication: success');
-			setTimeout(function(){ window.location = "/admin/"; }, 1000);
-	}
+    function successAuth(res) {
+        $localStorage.token = res.token;
+            console.log('authentication: success');
+            setTimeout(function() { window.location = '/admin/'; }, 1000);
+    }
 
-	$scope.login = function () {
-		var formData = {
-			email: $scope.user.email,
-			password: $scope.user.password
-		};
+    $scope.login = function() {
+        var formData = {
+            email: $scope.user.email,
+            password: $scope.user.password
+        };
 
-		authService.login(formData, successAuth, function () {
-			var message = {'data': {'message': 'Login/Senha incorreta, tente novamente.'}};
-			$scope.$broadcast('sendAlert', message);
-		});
+        authService.login(formData, successAuth, function() {
+            var message = {'data': {'message': 'Login/Senha incorreta, tente novamente.'}};
+            $scope.$broadcast('sendAlert', message);
+        });
 
-	};
+    };
 
-	$scope.logout = function () {
-		authService.logout(function () {
-			console.log('redirect logout');
-			setTimeout(function(){ window.location = "/admin/"; }, 1000);
+    $scope.logout = function() {
+        authService.logout(function() {
+            console.log('redirect logout');
+            setTimeout(function() { window.location = '/admin/'; }, 1000);
 
-		});
-	};
+        });
+    };
 
-	$scope.token = $localStorage.token;
-	$scope.tokenClaims = authService.getTokenClaims();
-	$scope.user = $scope.tokenClaims.data;
+    $scope.token = $localStorage.token;
+    $scope.tokenClaims = authService.getTokenClaims();
+    $scope.user = $scope.tokenClaims.data;
 
-	$scope.isAuth = function() {
-		if ( !empty($scope.tokenClaims.data) ) {
-			return true;
-		} else {
-			return false;
-		}
-	};
+    $scope.isAuth = function() {
+        if (!empty($scope.tokenClaims.data)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 
-	// get service function to be used async
-	if ( !empty($scope.tokenClaims.data) ) {
-		apiService.getEdges().then(function(edges) {
-			$scope.edges = edges;
-		});		
-	}
+    // get service function to be used async
+    if (!empty($scope.tokenClaims.data)) {
+        apiService.getEdges().then(function(edges) {
+            $scope.edges = edges;
+        });
+    }
 
 });
 
 // Dashboard Controller
-AdminApp.controller('DashboardController', function ($scope, hi, edges) {
-	$scope.hi = hi;
-	$scope.edges = edges;
+AdminApp.controller('DashboardController', function($scope, hi, edges) {
+    $scope.hi = hi;
+    $scope.edges = edges;
 });
 
 // Menu Controller
-AdminApp.controller('MenuController', function ($scope, $location) {
+AdminApp.controller('MenuController', function($scope, $location) {
 
-	$scope.isActive = function(edge) {
-		if ( ($location.path().split('/')[2] === edge) || ($location.path() === '/' && edge === 'dashboard') ) {
-			return 'active';
-		} else {
-			return '';
-		}
-	};
+    $scope.isActive = function(edge) {
+        if (($location.path().split('/')[2] === edge) || ($location.path() === '/' && edge === 'dashboard')) {
+            return 'active';
+        } else {
+            return '';
+        }
+    };
 
 });
 
 // LIST Controller
-AdminApp.controller('ListController', function ($scope, $location, $http, $routeParams, schema, list, count, config) {
-	
-	$scope.alert 			= {};
-	$scope.schema 			= schema;
-	$scope.items 			= list;
-	$scope.itemsThisPage 	= Object.keys(list).length; 
-	$scope.totalItems 		= count.sum;
-	$scope.itemsPerPage 	= 5;
-	$scope.maxSize 			= 10;
+AdminApp.controller('ListController', function($scope, $location, $http, $routeParams, schema, list, count, config) {
 
-	$scope.setPage = function (page) {
-		$scope.currentPage = page;
-	};
+    $scope.alert = {};
+    $scope.schema = schema;
+    $scope.items = list;
+    $scope.itemsThisPage = Object.keys(list).length;
+    $scope.totalItems = count.sum;
+    $scope.itemsPerPage = 5;
+    $scope.maxSize = 10;
 
-	$scope.setPage($routeParams.page);
+    $scope.setPage = function(page) {
+        $scope.currentPage = page;
+    };
 
-	$scope.pageChanged = function() {
-		$location.url('/list/'+ $routeParams.edge +'/p/'+$scope.currentPage);
-	};
+    $scope.setPage($routeParams.page);
 
-	$scope.onReload = function() {
-		window.location.reload();
-	};
+    $scope.pageChanged = function() {
+        $location.url('/list/' + $routeParams.edge + '/p/' + $scope.currentPage);
+    };
 
-	$scope.onExport = function() {
+    $scope.onReload = function() {
+        window.location.reload();
+    };
 
-		$http.get(config.API_BASE_URL + '/export/'+ $routeParams.edge, { responseType: 'arraybuffer' })
-			.success(function(data) {
-				var file = new Blob([data], { type: 'application/csv' });
-				var expTimestamp = Date.now();
-				saveAs(file, 'export-' + $routeParams.edge +'-' + expTimestamp + '.csv');
-		});
+    $scope.onExport = function() {
 
-	};
+        $http.get(config.API_BASE_URL + '/export/' + $routeParams.edge, { responseType: 'arraybuffer' })
+            .success(function(data) {
+                var file = new Blob([data], { type: 'application/csv' });
+                var expTimestamp = Date.now();
+                saveAs(file, 'export-' + $routeParams.edge + '-' + expTimestamp + '.csv');
+        });
 
-	$scope.onDestroy = function(id) {
-		var destroyItem = confirm('Tem certeza que deseja excluir?');
+    };
 
-		if (destroyItem) {
-			$http.delete(config.API_BASE_URL + '/destroy/'+ $routeParams.edge +'/'+id).then(function(response) {
-				$scope.$broadcast('sendAlert', response);
-				delete $scope.items[id];
-			});
-		}
-	}
+    $scope.onDestroy = function(id) {
+        var destroyItem = confirm('Tem certeza que deseja excluir?');
+
+        if (destroyItem) {
+            $http.delete(config.API_BASE_URL + '/destroy/' + $routeParams.edge + '/' + id).then(function(response) {
+                $scope.$broadcast('sendAlert', response);
+                delete $scope.items[id];
+            });
+        }
+    };
 
 });
 
 // CREATE Controller
-AdminApp.controller('CreateController', function ($scope, schema) {
+AdminApp.controller('CreateController', function($scope, schema) {
 
-	$scope.schema = schema;
-	$scope.schemaData = {};
+    $scope.schema = schema;
+    $scope.schemaData = {};
 
-	$scope.onChange = function(data) {
-		// fired onChange of form data. 
-		console.dir(data);
-	};
+    $scope.onChange = function(data) {
+        // fired onChange of form data.
+        console.dir(data);
+    };
 
 });
 
 // READ Controller
-AdminApp.controller('ReadController', function ($scope, schema, read) {
-	
-	$scope.item = read;
-	$scope.schema = schema;
-	$scope.schemaData = read;
+AdminApp.controller('ReadController', function($scope, schema, read) {
 
-	$scope.onLoad = function() {
-		$scope.$broadcast('disableForm', {});
-	};
+    $scope.item = read;
+    $scope.schema = schema;
+    $scope.schemaData = read;
+
+    $scope.onLoad = function() {
+        $scope.$broadcast('disableForm', {});
+    };
 
 });
 
 // UPDATE Controller
-AdminApp.controller('UpdateController', function ($scope, schema, read) {
-	
-	$scope.item = read;
-	$scope.schema = schema;
-	$scope.schemaData = read;
+AdminApp.controller('UpdateController', function($scope, schema, read) {
 
-	$scope.onChange = function(data) {
-		// fired onChange of form data. 
-		console.dir(data);
-	};
+    $scope.item = read;
+    $scope.schema = schema;
+    $scope.schemaData = read;
+
+    $scope.onChange = function(data) {
+        // fired onChange of form data.
+        console.dir(data);
+    };
 
 });
 
 // Update Password Controller
-AdminApp.controller('UpdatePasswordController', function ($scope) {
+AdminApp.controller('UpdatePasswordController', function($scope) {
 
-	// TODO: would be nice to validate if new_password and confirm_new_password are equal values thru JSON Schema frontend.	
-	var schema = 
-		{	"type": "object",
-			"required": true,
-			"properties": {
-				"password": {"type": "string", "format": "password", "title": "Senha Atual", "required": true, "minLength": 1, "maxLength": 191 },
-				"new_password": {"type": "string", "format": "password", "title": "Nova Senha", "required": true, "minLength": 1, "maxLength": 191 },
-				"confirm_new_password": {"type": "string", "format": "password", "title": "Confirme Nova Senha", "required": true, "minLength": 1, "maxLength": 191 },
-			},
-		};
+    // TODO: would be nice to validate if new_password and confirm_new_password are equal values thru JSON Schema frontend.
+    var schema =
+        { 'type': 'object',
+            'required': true,
+            'properties': {
+                'password': {'type': 'string', 'format': 'password', 'title': 'Senha Atual', 'required': true, 'minLength': 1, 'maxLength': 191 },
+                'new_password': {'type': 'string', 'format': 'password', 'title': 'Nova Senha', 'required': true, 'minLength': 1, 'maxLength': 191 },
+                'confirm_new_password': {'type': 'string', 'format': 'password', 'title': 'Confirme Nova Senha', 'required': true, 'minLength': 1, 'maxLength': 191 },
+            },
+        };
 
-	$scope.schema = schema;
-	$scope.schemaData = {};
+    $scope.schema = schema;
+    $scope.schemaData = {};
 
-	$scope.onChange = function(data) {
-		// fired onChange of form data. 
-		console.dir(data);
-	};
+    $scope.onChange = function(data) {
+        // fired onChange of form data.
+        console.dir(data);
+    };
 
 });
 
 // Form Controller
-AdminApp.controller('FormController', function ($scope, $http, $location, $routeParams, config) {
-	
-	var edge 	= $routeParams.edge;
-	var id 		= $routeParams.id;
+AdminApp.controller('FormController', function($scope, $http, $location, $routeParams, config) {
 
-	$scope.schema = $scope.$parent.schema;
-	$scope.itemId = id;
+    var edge = $routeParams.edge;
+    var id = $routeParams.id;
 
-	if($scope.$parent.read !== undefined){
-		$scope.$on('disableForm', function(event, obj) {
+    $scope.schema = $scope.$parent.schema;
+    $scope.itemId = id;
 
-			$scope.editor.disable();
+    if ($scope.$parent.read !== undefined) {
+        $scope.$on('disableForm', function(event, obj) {
 
-			// TODO: when using sceditor WYSIWYG, need to fire function to "readOnly = true"
-			// Examples that doesn't work: 
-			// instance.readOnly(1);
-			// $scope.editor.plugins.sceditor.readOnly(true);
-			
-		});
-	};
+            $scope.editor.disable();
 
-	$scope.onCreate = function() {
+            // TODO: when using sceditor WYSIWYG, need to fire function to "readOnly = true"
+            // Examples that doesn't work:
+            // instance.readOnly(1);
+            // $scope.editor.plugins.sceditor.readOnly(true);
 
-		var item = $scope.editor.getValue();
-		$http.post(config.API_BASE_URL + '/create/'+edge, item).success(function(){
-			$location.path('/list/'+edge);
-		});
+        });
+    }
 
-	};
+    $scope.onCreate = function() {
 
-	$scope.onUpdate = function(){
-	
-		var item = $scope.editor.getValue();
-		$http.put(config.API_BASE_URL + '/update/'+edge+'/'+id, item).success(function() {
-			$location.path('/list/'+edge);
-		});
-	};
+        var item = $scope.editor.getValue();
+        $http.post(config.API_BASE_URL + '/create/' + edge, item).success(function() {
+            $location.path('/list/' + edge);
+        });
 
-	$scope.onUpdatePassword = function(){
-		var item = $scope.editor.getValue();
-		var id = $scope.$parent.user.id;
-		
-		$http.put(config.API_BASE_URL + '/updatePassword/user/'+id, item).success(function() {
-			console.log('PUT!');
-			//$location.path('/list/'+edge);
-		});
-	};
+    };
 
-	$scope.onDestroy = function() {
-		var destroyItem = confirm('Tem certeza que deseja excluir?');
+    $scope.onUpdate = function() {
+        var item = $scope.editor.getValue();
+        $http.put(config.API_BASE_URL + '/update/' + edge + '/' + id, item).success(function() {
+            $location.path('/list/' + edge);
+        });
+    };
 
-		if (destroyItem) {
-			$http.delete(config.API_BASE_URL + '/destroy/'+edge+'/'+id).then(function(response) {
-				$location.path('/list/'+edge);
-			});
-		}
-	}
+    $scope.onUpdatePassword = function() {
+        var item = $scope.editor.getValue();
+        var id = $scope.$parent.user.id;
+
+        $http.put(config.API_BASE_URL + '/updatePassword/user/' + id, item).success(function() {
+            console.log('PUT!');
+            //$location.path('/list/'+edge);
+        });
+    };
+
+    $scope.onDestroy = function() {
+        var destroyItem = confirm('Tem certeza que deseja excluir?');
+
+        if (destroyItem) {
+            $http.delete(config.API_BASE_URL + '/destroy/' + edge + '/' + id).then(function(response) {
+                $location.path('/list/' + edge);
+            });
+        }
+    };
 
 });
 
-AdminApp.controller('AlertController', function ($scope) {
-	$scope.alerts = [];
+AdminApp.controller('AlertController', function($scope) {
+    $scope.alerts = [];
 
-	$scope.$on('sendAlert', function(event, obj) {
-		$scope.alerts.push({type: 'danger', msg: obj.data.message});		
-	});
+    $scope.$on('sendAlert', function(event, obj) {
+        $scope.alerts.push({type: 'danger', msg: obj.data.message});
+    });
 
-	$scope.closeAlert = function(index) {
-		$scope.alerts.splice(index, 1);
-	};
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
 
 });
--- www/admin/assets/js/app.services.js
+++ www/admin/assets/js/app.services.js
@@ -1,214 +1,214 @@
 /* ************************************************************
 ANGULAR ADMIN APP SERVICES
-************************************************************ */		
+************************************************************ */
 
 // authService Factory
-AdminApp.factory('authService', function ($http, $localStorage, $location, config) {
+AdminApp.factory('authService',
+    function($http, $localStorage, $location, config) {
 
-	function urlBase64Decode(str) {
-		var output = str.replace('-', '+').replace('_', '/');
-		switch (output.length % 4) {
-			case 0:
-				break;
-			case 2:
-				output += '==';
-				break;
-			case 3:
-				output += '=';
-				break;
-			default:
-				throw 'Illegal base64url string!';
-		}
-		return window.atob(output);
-	}
+    function urlBase64Decode(str) {
+        var output = str.replace('-', '+').replace('_', '/');
+        switch (output.length % 4) {
+            case 0:
+                break;
+            case 2:
+                output += '==';
+                break;
+            case 3:
+                output += '=';
+                break;
+            default:
+                throw 'Illegal base64url string!';
+        }
+        return window.atob(output);
+    }
 
-	function getClaimsFromToken() {
-		var token = $localStorage.token;
-		var user = {};
-		if (typeof token !== 'undefined') {
-			var encoded = token.split('.')[1];
-			user = JSON.parse(urlBase64Decode(encoded));
-		}
-		return user;
-	}
+    function getClaimsFromToken() {
+        var token = $localStorage.token;
+        var user = {};
+        if (typeof token !== 'undefined') {
+            var encoded = token.split('.')[1];
+            user = JSON.parse(urlBase64Decode(encoded));
+        }
+        return user;
+    }
 
-	var tokenClaims = getClaimsFromToken();
+    var tokenClaims = getClaimsFromToken();
 
-	return {
-		isAuth: function() {
+    return {
+        isAuth: function() {
 
-			if(empty(tokenClaims)){
-				$location.url('/login');
-			};
+            if (empty(tokenClaims)) {
+                $location.url('/login');
+            }
 
-			if(!empty(tokenClaims) && $location.path() == '/'){
-				$location.url('/dashboard');				
-			};
+            if (!empty(tokenClaims) && $location.path() == '/') {
+                $location.url('/dashboard');
+            }
 
-		},
-		login: function (data, success, error) {
-			$http.post(config.API_SIGNIN_URL, data).success(success).error(error)
-		},
-		logout: function (success) {
-			tokenClaims = {};
-			delete $localStorage.token;
-			success();
-		},
-		getTokenClaims: function () {
-			return tokenClaims;
-		}
-	};
+        },
+        login: function(data, success, error) {
+            $http.post(config.API_SIGNIN_URL, data).success(success).error(error);
+        },
+        logout: function(success) {
+            tokenClaims = {};
+            delete $localStorage.token;
+            success();
+        },
+        getTokenClaims: function() {
+            return tokenClaims;
+        }
+    };
 });
 
 // apiService Factory
-AdminApp.factory("apiService", function ($q, $http, $location, $route, config) {
+AdminApp.factory('apiService', function($q, $http, $location, $route, config) {
 
-	var apiService = {
+    var apiService = {
 
-		getHi: function() {
-			var deferred = $q.defer();
-			
-			hi = $http.get(config.API_BASE_URL + '/hi').then(function(response) {
-				deferred.resolve(response.data);
-			});
+        getHi: function() {
+            var deferred = $q.defer();
 
-			return deferred.promise;
-		},
+            hi = $http.get(config.API_BASE_URL + '/hi').then(function(response) {
+                deferred.resolve(response.data);
+            });
 
-		validateParams: function() {
+            return deferred.promise;
+        },
 
-			// define variables
-			var deferred = $q.defer();
-			var edge 	= $route.current.params.edge;
-			var page 	= $route.current.params.page;
-			var id 		= $route.current.params.id;
+        validateParams: function() {
 
-			edges = apiService.getEdges().then(function(edges) {
+            // define variables
+            var deferred = $q.defer();
+            var edge = $route.current.params.edge;
+            var page = $route.current.params.page;
+            var id = $route.current.params.id;
 
-				// validate if edge exist in beans
-				if (edges[edge]){
+            edges = apiService.getEdges().then(function(edges) {
 
-					// validate if id param is required
-					if(id !== undefined){
-						
-						idCheck = $http.get(config.API_BASE_URL + '/exists/'+edge+'/'+id).then(function(response) {
+                // validate if edge exist in beans
+                if (edges[edge]) {
 
-							// validate if ID exist in database					
-							if(response.data.exists === true){
-								deferred.resolve();
-							}
-							else{
-								deferred.reject('ID dos not exist');
-								console.log('ID does not exist.');
-								$location.url('/');
-							}
+                    // validate if id param is required
+                    if (id !== undefined) {
+                        idCheck = $http.get(config.API_BASE_URL + '/exists/' + edge + '/' + id).then(function(response) {
 
-						});
-					}
-					else{
-						deferred.resolve();
-					}
+                            // validate if ID exist in database
+                            if (response.data.exists === true) {
+                                deferred.resolve();
+                            }
+                            else {
+                                deferred.reject('ID dos not exist');
+                                console.log('ID does not exist.');
+                                $location.url('/');
+                            }
 
-				}
-				else{
-					deferred.reject('Edge dos not exist');
-					console.log('Edge dos not exist.');
-					$location.url('/');
-				}
-			});
+                        });
+                    }
+                    else {
+                        deferred.resolve();
+                    }
 
-			return deferred.promise;
-		},
+                }
+                else {
+                    deferred.reject('Edge dos not exist');
+                    console.log('Edge dos not exist.');
+                    $location.url('/');
+                }
+            });
 
-		getEdges: function() {
-			var deferred = $q.defer();
+            return deferred.promise;
+        },
 
-			edges = $http.get(config.API_BASE_URL + '/edges').then(function(response) {
-				deferred.resolve(response.data.beans);
-			});
-			
-			return deferred.promise;
-		},
+        getEdges: function() {
+            var deferred = $q.defer();
 
-		getSchema: function() {
-			var deferred = $q.defer();
-			var edge = $route.current.params.edge;
+            edges = $http.get(config.API_BASE_URL + '/edges').then(function(response) {
+                deferred.resolve(response.data.beans);
+            });
 
-			schema = $http.get(config.API_BASE_URL + '/schema/'+edge).then(function(response) {
-				deferred.resolve(response.data);
-			});
-			
-			return deferred.promise;
-		},
+            return deferred.promise;
+        },
 
-		getList: function() {
-			var deferred = $q.defer();
-			var edge = $route.current.params.edge;
-			var page = $route.current.params.page;
+        getSchema: function() {
+            var deferred = $q.defer();
+            var edge = $route.current.params.edge;
 
-			list = $http.get(config.API_BASE_URL + '/list/'+edge+'/'+page).then(function(response) {
-				deferred.resolve(response.data);
-			});
-			
-			return deferred.promise;
-		},
+            schema = $http.get(config.API_BASE_URL + '/schema/' + edge).then(function(response) {
+                deferred.resolve(response.data);
+            });
 
-		getCount: function() {
-			var deferred = $q.defer();
-			var edge = $route.current.params.edge;
+            return deferred.promise;
+        },
 
-			count = $http.get(config.API_BASE_URL + '/count/'+edge).then(function(response) {
-				deferred.resolve(response.data);
-			});
-			
-			return deferred.promise;
-		},
+        getList: function() {
+            var deferred = $q.defer();
+            var edge = $route.current.params.edge;
+            var page = $route.current.params.page;
 
-		getRead: function() {
-			var deferred = $q.defer();
-			var edge 	= $route.current.params.edge;
-			var id 		= $route.current.params.id;
+            list = $http.get(config.API_BASE_URL + '/list/' + edge + '/' + page).then(function(response) {
+                deferred.resolve(response.data);
+            });
 
-			read = $http.get(config.API_BASE_URL + '/read/'+edge+'/'+id).then(function(response) {
-				deferred.resolve(response.data);
-			});
-			
-			return deferred.promise;
-		},
+            return deferred.promise;
+        },
 
-	};
+        getCount: function() {
+            var deferred = $q.defer();
+            var edge = $route.current.params.edge;
 
-	return apiService;
+            count = $http.get(config.API_BASE_URL + '/count/' + edge).then(function(response) {
+                deferred.resolve(response.data);
+            });
+
+            return deferred.promise;
+        },
+
+        getRead: function() {
+            var deferred = $q.defer();
+            var edge = $route.current.params.edge;
+            var id = $route.current.params.id;
+
+            read = $http.get(config.API_BASE_URL + '/read/' + edge + '/' + id).then(function(response) {
+                deferred.resolve(response.data);
+            });
+
+            return deferred.promise;
+        },
+
+    };
+
+    return apiService;
 });
 
 /* ************************************************************
 ANGULAR ADMIN APP INTERCEPTORS
-************************************************************ */		
+************************************************************ */
 
 // apiInterceptor Factory
-AdminApp.factory('apiInterceptor', ['$q', '$location', '$localStorage', function($q, $location, $localStorage) {  
+AdminApp.factory('apiInterceptor', ['$q', '$location', '$localStorage', function($q, $location, $localStorage) {
 
-	var apiInterceptor = {
-		'request': function (config) {
-			config.headers = config.headers || {};
-			if ($localStorage.token) {
-				config.headers.Authorization = 'Bearer ' + $localStorage.token;
-			}
-			return config;
-		},
-		'responseError': function (response) {
-			if (response.status === 400 || response.status === 401 || response.status === 403) {
+    var apiInterceptor = {
+        'request': function(config) {
+            config.headers = config.headers || {};
+            if ($localStorage.token) {
+                config.headers.Authorization = 'Bearer ' + $localStorage.token;
+            }
+            return config;
+        },
+        'responseError': function(response) {
+            if (response.status === 400 || response.status === 401 || response.status === 403) {
 
-				if( $location.path() !== '/login' && typeof reloadLock === 'undefined' ){
-					reloadLock = true;
-					delete $localStorage.token;
-					tokenClaims = {};
-					window.location.href = window.location.pathname;
-				};
-			}
-			return $q.reject(response);
-		}
-	};
+                if ($location.path() !== '/login' && typeof reloadLock === 'undefined') {
+                    reloadLock = true;
+                    delete $localStorage.token;
+                    tokenClaims = {};
+                    window.location.href = window.location.pathname;
+                }
+            }
+            return $q.reject(response);
+        }
+    };
 
     return apiInterceptor;
 }]);
