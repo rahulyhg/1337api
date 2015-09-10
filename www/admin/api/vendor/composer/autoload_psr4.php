d719951cd324973cd2a111cc4edf6daaed9cf11a
Elijah Hatem <elias.hatem@gmail.com>
2015-09-10 10:35:51 -0300

    added `sinergi/dictionary` php library to composer.

--- www/admin/api/vendor/composer/autoload_psr4.php
+++ www/admin/api/vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Sinergi\\Dictionary\\' => array($vendorDir . '/sinergi/dictionary/src/Dictionary'),
     'Firebase\\JWT\\' => array($vendorDir . '/firebase/php-jwt/src'),
 );
