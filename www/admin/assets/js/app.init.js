3ad81ab3aa3fd266366557504dbff19678abdbcf
Elijah Vs Jesus <elias.hatem@gmail.com>
2015-09-13 17:45:19 -0300

    random sublime sh*t

--- www/admin/assets/js/app.init.js
+++ www/admin/assets/js/app.init.js
@@ -8,11 +8,11 @@ ANGULAR CONSTANTS
 ************************************************************ */
 
 AdminApp.constant(
-    'config',
-        {
-            API_BASE_URL: 'api/private',
-            API_SIGNIN_URL: 'api/auth',
-        }
+	'config',
+		{
+			API_BASE_URL: 'api/private',
+			API_SIGNIN_URL: 'api/auth',
+		}
 );
 
 /* ************************************************************
@@ -20,133 +20,133 @@ ANGULAR CONFIG - NG ROUTES PROVIDER
 ************************************************************ */
 
 AdminApp.config([
-    '$routeProvider',
-    function($routeProvider) {
+	'$routeProvider',
+	function($routeProvider) {
 
-        // DASHBOARD
-        $routeProvider.when(
-            '/',
-            {
-                controller: 'MainController',
-                controllerAs: 'main',
-                resolve: {
-                    auth: function(authService) { return authService.isAuth(); },
-                }
-            }
-        );
+		// DASHBOARD
+		$routeProvider.when(
+			'/',
+			{
+				controller: 'MainController',
+				controllerAs: 'main',
+				resolve: {
+					auth: function(authService) { return authService.isAuth(); },
+				}
+			}
+		);
 
-        // DASHBOARD
-        $routeProvider.when(
-            '/dashboard',
-            {
-                templateUrl: 'assets/tpl/dashboard.html',
-                controller: 'DashboardController',
-                controllerAs: 'dashboard',
-                resolve: {
-                    auth: function(authService) { return authService.isAuth(); },
-                    hi: function(apiService) { return apiService.getHi(); },
-                    edges: function(apiService) { return apiService.getEdges(); },
-                }
-            }
-        );
+		// DASHBOARD
+		$routeProvider.when(
+			'/dashboard',
+			{
+				templateUrl: 'assets/tpl/dashboard.html',
+				controller: 'DashboardController',
+				controllerAs: 'dashboard',
+				resolve: {
+					auth: function(authService) { return authService.isAuth(); },
+					hi: function(apiService) { return apiService.getHi(); },
+					edges: function(apiService) { return apiService.getEdges(); },
+				}
+			}
+		);
 
-        // LOGIN
-        $routeProvider.when(
-            '/login',
-            {
-                templateUrl: 'assets/tpl/login.html',
-                controller: 'MainController',
-                controllerAs: 'main',
-            }
-        );
+		// LOGIN
+		$routeProvider.when(
+			'/login',
+			{
+				templateUrl: 'assets/tpl/login.html',
+				controller: 'MainController',
+				controllerAs: 'main',
+			}
+		);
 
-        // UPDATE PASSWORD
-        $routeProvider.when(
-            '/userAccount/updatePassword',
-            {
-                templateUrl: 'assets/tpl/updatePassword.html',
-                controller: 'UpdatePasswordController',
-                controllerAs: 'updatePassword',
-                resolve: {
-                    auth: function(authService) { return authService.isAuth(); },
-                },
-            }
-        );
+		// UPDATE PASSWORD
+		$routeProvider.when(
+			'/userAccount/updatePassword',
+			{
+				templateUrl: 'assets/tpl/updatePassword.html',
+				controller: 'UpdatePasswordController',
+				controllerAs: 'updatePassword',
+				resolve: {
+					auth: function(authService) { return authService.isAuth(); },
+				},
+			}
+		);
 
-        // CRUD
-        $routeProvider.when(
-            '/list/:edge/p/:page',
-            {
-                templateUrl: 'assets/tpl/list.html',
-                controller: 'ListController',
-                controllerAs: 'list',
-                resolve: {
-                    auth: function(authService) { return authService.isAuth(); },
-                    valid: function(apiService) { return apiService.validateParams(); },
-                    schema: function(apiService) { return apiService.getSchema(); },
-                    list: function(apiService) { return apiService.getList(); },
-                    count: function(apiService) { return apiService.getCount(); },
-                }
-            }
-        );
+		// CRUD
+		$routeProvider.when(
+			'/list/:edge/p/:page',
+			{
+				templateUrl: 'assets/tpl/list.html',
+				controller: 'ListController',
+				controllerAs: 'list',
+				resolve: {
+					auth: function(authService) { return authService.isAuth(); },
+					valid: function(apiService) { return apiService.validateParams(); },
+					schema: function(apiService) { return apiService.getSchema(); },
+					list: function(apiService) { return apiService.getList(); },
+					count: function(apiService) { return apiService.getCount(); },
+				}
+			}
+		);
 
-        $routeProvider.when(
-            '/create/:edge',
-            {
-                templateUrl: 'assets/tpl/create.html',
-                controller: 'CreateController',
-                controllerAs: 'create',
-                resolve: {
-                    auth: function(authService) { return authService.isAuth(); },
-                    valid: function(apiService) { return apiService.validateParams(); },
-                    schema: function(apiService) { return apiService.getSchema(); },
-                }
-            }
-        );
+		$routeProvider.when(
+			'/create/:edge',
+			{
+				templateUrl: 'assets/tpl/create.html',
+				controller: 'CreateController',
+				controllerAs: 'create',
+				resolve: {
+					auth: function(authService) { return authService.isAuth(); },
+					valid: function(apiService) { return apiService.validateParams(); },
+					schema: function(apiService) { return apiService.getSchema(); },
+				}
+			}
+		);
 
-        $routeProvider.when(
-            '/read/:edge/:id',
-            {
-                templateUrl: 'assets/tpl/read.html',
-                controller: 'ReadController',
-                controllerAs: 'read',
-                resolve: {
-                    auth: function(authService) { return authService.isAuth(); },
-                    valid: function(apiService) { return apiService.validateParams(); },
-                    schema: function(apiService) { return apiService.getSchema(); },
-                    read: function(apiService) { return apiService.getRead(); },
-                },
-            }
-        );
+		$routeProvider.when(
+			'/read/:edge/:id',
+			{
+				templateUrl: 'assets/tpl/read.html',
+				controller: 'ReadController',
+				controllerAs: 'read',
+				resolve: {
+					auth: function(authService) { return authService.isAuth(); },
+					valid: function(apiService) { return apiService.validateParams(); },
+					schema: function(apiService) { return apiService.getSchema(); },
+					read: function(apiService) { return apiService.getRead(); },
+				},
+			}
+		);
 
-        $routeProvider.when(
-            '/update/:edge/:id',
-            {
-                templateUrl: 'assets/tpl/update.html',
-                controller: 'UpdateController',
-                controllerAs: 'update',
-                resolve: {
-                    auth: function(authService) { return authService.isAuth(); },
-                    valid: function(apiService) { return apiService.validateParams(); },
-                    schema: function(apiService) { return apiService.getSchema(); },
-                    read: function(apiService) { return apiService.getRead(); },
-                },
-            }
-        );
+		$routeProvider.when(
+			'/update/:edge/:id',
+			{
+				templateUrl: 'assets/tpl/update.html',
+				controller: 'UpdateController',
+				controllerAs: 'update',
+				resolve: {
+					auth: function(authService) { return authService.isAuth(); },
+					valid: function(apiService) { return apiService.validateParams(); },
+					schema: function(apiService) { return apiService.getSchema(); },
+					read: function(apiService) { return apiService.getRead(); },
+				},
+			}
+		);
 
-        // REDIRECTS
-        $routeProvider.when(
-            '/list/:edge',
-            {
-                redirectTo: function(routeParams, path, search) { return path + '/p/1'; }
-            }
-        );
+		// REDIRECTS
+		$routeProvider.when(
+			'/list/:edge',
+			{
+				redirectTo: function(routeParams, path, search) { return path + '/p/1'; }
+			}
+		);
 
-        // OTHERWISE
-        $routeProvider.otherwise(
-            {redirectTo: '/'}
-        );
-    }
+		// OTHERWISE
+		$routeProvider.otherwise(
+			{redirectTo: '/'}
+		);
+	}
 ]);
 
 /* ************************************************************
@@ -154,9 +154,9 @@ ANGULAR CONFIG - HTTP PROVIDER
 ************************************************************ */
 
 AdminApp.config([
-    '$httpProvider',
-    function($httpProvider) {
-        $httpProvider.interceptors.push('apiInterceptor');
+	'$httpProvider',
+	function($httpProvider) {
+		$httpProvider.interceptors.push('apiInterceptor');
 }]);
 
 /* ************************************************************
@@ -168,77 +168,77 @@ ANGULAR JSON EDITOR
 
 var jeUploadFunction = function(type, file, cbs) {
 
-                        if (file) {
-                            var reader = new FileReader();
+						if (file) {
+							var reader = new FileReader();
 
-                            reader.onloadend = function(evt) {
-                                var b = evt.target.result;
-                                var uploadData = '{"filename":"' + file.name + '", "filesize":"' + file.size + '", "blob":"' + b + '"}';
-                                var percentComplete = 0;
+							reader.onloadend = function(evt) {
+								var b = evt.target.result;
+								var uploadData = '{"filename":"' + file.name + '", "filesize":"' + file.size + '", "blob":"' + b + '"}';
+								var percentComplete = 0;
 
-                                    $.ajax({
-                                        xhr: function() {
-                                            var xhr = new window.XMLHttpRequest();
-                                            xhr.upload.addEventListener('progress', function(evt) {
-                                                if (evt.lengthComputable) {
-                                                    var percentComplete = Math.round((evt.loaded / evt.total) * 100);
-                                                    if (percentComplete <= 100) {
-                                                        cbs.updateProgress(percentComplete);
-                                                    }
-                                                }
-                                            }, false);
-                                            return xhr;
-                                        },
-                                        type: 'POST',
-                                        // TODO: need to pass "edge" at upload url.
-                                        url: 'api/private/upload/page',
-                                        contentType: 'application/json; charset=utf-8',
-                                        dataType: 'json',
-                                        data: uploadData,
+									$.ajax({
+										xhr: function() {
+											var xhr = new window.XMLHttpRequest();
+											xhr.upload.addEventListener('progress', function(evt) {
+												if (evt.lengthComputable) {
+													var percentComplete = Math.round((evt.loaded / evt.total) * 100);
+													if (percentComplete <= 100) {
+														cbs.updateProgress(percentComplete);
+													}
+												}
+											}, false);
+											return xhr;
+										},
+										type: 'POST',
+										// TODO: need to pass "edge" at upload url.
+										url: 'api/private/upload/page',
+										contentType: 'application/json; charset=utf-8',
+										dataType: 'json',
+										data: uploadData,
 
-                                        success: function(data, textStatus, jQxhr) {
-                                            cbs.success('' + data.id + '');
-                                        },
-                                        error: function(jqXhr, textStatus, errorThrown) {
-                                            console.log(errorThrown);
-                                        }
-                                    });
+										success: function(data, textStatus, jQxhr) {
+											cbs.success('' + data.id + '');
+										},
+										error: function(jqXhr, textStatus, errorThrown) {
+											console.log(errorThrown);
+										}
+									});
 
-                            };
-                            reader.readAsDataURL(file);
-                        }
-                    };
+							};
+							reader.readAsDataURL(file);
+						}
+					};
 
 // GLOBAL JSON EDITOR CONFIG
 // **************************** */
 
 var jePluginsConfig = {
-                        sceditor: {
-                            style: 'assets/css/libs/jquery.sceditor.default.min.css',
-                            toolbar: 'bold,italic,underline|strike,subscript,superscript|link,unlink|removeformat|bulletlist,orderedlist|source',
-                            locale: 'pt-BR',
-                            emoticonsEnabled: false,
-                            width: '98%',
-                            resizeEnabled: false,
-                        }
-                    };
+						sceditor: {
+							style: 'assets/css/libs/jquery.sceditor.default.min.css',
+							toolbar: 'bold,italic,underline|strike,subscript,superscript|link,unlink|removeformat|bulletlist,orderedlist|source',
+							locale: 'pt-BR',
+							emoticonsEnabled: false,
+							width: '98%',
+							resizeEnabled: false,
+						}
+					};
 
 var jeOptionsConfig = {
-                        iconlib: 'fontawesome4',
-                        theme: 'bootstrap3',
-                        ajax: true,
-                        disable_collapse: true,
-                        disable_edit_json: true,
-                        disable_properties: true,
-                        upload: jeUploadFunction,
-                    };
+						iconlib: 'fontawesome4',
+						theme: 'bootstrap3',
+						ajax: true,
+						disable_collapse: true,
+						disable_edit_json: true,
+						disable_properties: true,
+						upload: jeUploadFunction,
+					};
 
 // JSON Editor Provider
 // **************************** */
 
 AdminApp.config(function(JSONEditorProvider) {
-    JSONEditorProvider.configure({
-        plugins: jePluginsConfig,
-        defaults: { options: jeOptionsConfig }
-    });
+	JSONEditorProvider.configure({
+		plugins: jePluginsConfig,
+		defaults: { options: jeOptionsConfig }
+	});
 });
