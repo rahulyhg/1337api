43cf6f599622f3f5a118977aa73bfd101ea3626f
Elijah Hatem <elias.hatem@gmail.com>
2015-09-16 16:59:30 -0300

    Better AUTH validation +++ organisation, code cleaning, pixel brushing and comment.

--- admin/api/index.php
+++ admin/api/index.php
@@ -7,18 +7,21 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/config.php';
 require __DIR__ . '/helpers.php';
 
+// REDBEAN ORM SETUP
 R::setup($config['db']['host'], $config['db']['user'], $config['db']['pass']);
 R::setAutoResolve( TRUE );
+R::freeze( FALSE );
 
 // TEST DB CONNECTION
-$conn = R::testConnection();
-
-if(!$conn){
+if(R::testConnection() == FALSE){
 	api_error('DB_CONN_FAIL');
-}
+	exit();
+};
 
+// INSPECT TABLES
 $config['api']['beans'] = R::inspect();
 
+// REDBEAN ORM DEBUG MODE ON
 if($config['api']['debug']){
 	R::debug( TRUE, 0 );
 };
@@ -27,6 +30,7 @@ if($config['api']['debug']){
 ** API REQUEST ***************************************************************************************
 *************************************************************************************************** */ 
 
+// BUILD $REQ OBJ FROM SERVER $_REQUEST
 foreach ($_REQUEST as $k => $v) {
 	$req[$k] = $v;
 };
@@ -35,6 +39,7 @@ foreach ($_REQUEST as $k => $v) {
 ** API REQUEST MODE **********************************************************************************
 *************************************************************************************************** */ 
 
+// SWITCH ROUTER FOR REQUEST MODE
 switch ($req['mode']) {
 	case 'auth':
 		require 'auth.php';
@@ -54,27 +59,20 @@ switch ($req['mode']) {
 ** API OUTPUT FUNCTIONS ******************************************************************************
 *************************************************************************************************** */ 
 
+// FORBIDDEN OUTPUT
 function api_forbid(){
 	header('HTTP/1.0 400 Bad Request');
-	
-	$res = array(
-		'error' => true, 
-		'message' => getMessage('INVALID_REQUEST')
-	);
+	$res = array('error' => true, 'message' => getMessage('INVALID_REQUEST'));
 	api_output($res);
 };
 
+// ERROR OUTPUT
 function api_error($msg){
-
-	$res = array(
-		'error' 	=> true,
-		'message' 	=> getMessage($msg)
-	);
-
+	$res = array('error' => true, 'message' => getMessage($msg));
 	api_output($res);
 };
 
-// OUTPUT
+// API JSON OUTPUT
 function api_output($res){
 	echo json_encode($res);
 };
