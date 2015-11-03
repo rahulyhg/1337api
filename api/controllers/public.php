17138796fb9a086ca3ef7af9ee2db3ff070885f8
Elijah Vs Jesus <elias.hatem@gmail.com>
2015-11-03 00:56:23 -0200

    converting raw php to `klein` method controller syntax.

--- api/controllers/public.php
+++ api/controllers/public.php
@@ -1,29 +1,14 @@
 <?php
 
-function api_hi(){
-	global $caption;
-
-	if(!empty($caption['messages'])) {
-		$res = array('message' => getMessage('HI'));
-	} 
-	else{
-		$res = array('error' => true, 'message' => 'Arquivo de mensagens não encontrado.');
-	}
-
-	return $res;
-
-	//output response
-	api_output($res);
-
-};
-
-
 /* ***************************************************************************************************
 ** COMING SOON ***************************************************************************************
 *************************************************************************************************** */ 
-	$res['message'] = 'elijah says: public API still in development.';
 
-	// OUTPUT
-	api_output($res);
+$this->respond('GET', '*',
+	function ($request, $response) {
+		$data['message'] = 'elijah says: public API still in development.';
+		$response->json($data);
+	}
+);
 
 ?>
