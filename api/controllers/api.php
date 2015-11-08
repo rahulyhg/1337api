c5a3aa241bad9df4ae0f00859102bf8c428eb900
Elijah Vs Jesus <elias.hatem@gmail.com>
2015-11-08 01:47:31 -0200

    error validation and exceptions for api_destroy

--- api/controllers/api.php
+++ api/controllers/api.php
@@ -725,12 +725,35 @@ function api_updatePassword ($request, $response, $args) {
 
 function api_destroy ($request, $response, $args) {
 
+	// check relationships, if exists
+	$hierarchyArr = R::getAll('
+		SELECT TABLE_NAME as child, REFERENCED_TABLE_NAME as parent
+		FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+		WHERE REFERENCED_TABLE_NAME = "'.$args['edge'].'"
+	');
+
+	if( !empty($hierarchyArr) ) {
+
+		foreach ($hierarchyArr as $k => $v) {
+			
+			$childs = R::getAll('
+				SELECT * FROM '.$v['child'].' WHERE `'.$v['parent'].'_id` = '.$args['id'].'
+			');
+			if ( !empty($childs) ) {
+				$err = array('error' => true, 'message' => getMessage('DESTROY_FAIL_CHILD_EXISTS'));
+				return $response->withJson($err)->withStatus(400);
+			}	
+		}
+	}
+
+	// no relationship? let's go on:
+
+	// dispense 'edge'
+	$item = R::load( $args['edge'], $args['id'] );
+
+	// let's start the delete transaction
 	R::begin();
 	try {
-		// dispense 'edge'
-		$id = $args['id'];
-		$item = R::load( $args['edge'], $id );
-		$formData = $request->getParsedBody();
 
 		// destroy item, commit if success
 	    R::trash($item);
@@ -738,16 +761,19 @@ function api_destroy ($request, $response, $args) {
 
 		// build api response array
 		$payload = array(
-			'id' 		=> $id,
-			'message' 	=> getMessage('DESTROY_SUCCESS') . ' (id: '.$id.')',
+			'id' 		=> $args['id'],
+			'message' 	=> getMessage('DESTROY_SUCCESS') . ' (id: '.$args['id'].')',
 		);
 
 		// output response
 		return $response->withJson($payload);
 
 	} catch (Exception $e) {
+
+		// rollback transaction
 		R::rollback();
-		api_error($response, 'DESTROY_FAIL', $e->getMessage());		
+
+		throw $e;
 	}
 };
 
