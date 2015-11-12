acc7ad9b3c06ea7a651c60683f95d31cf2d9a95a
Elijah Hatem <elias.hatem@gmail.com>
2015-11-12 12:21:16 -0200

    `refactor`: using `fputcsv` from PHP 5.5 instead of external vendor library `goodby/csv`.

--- api/app/src/Api.php
+++ api/app/src/Api.php
@@ -2,8 +2,6 @@
 namespace SlimBean;
 
 use \RedBeanPHP\Facade as R;
-use Goodby\CSV\Export\Standard\Exporter;
-use Goodby\CSV\Export\Standard\ExporterConfig;
 use Psr\Log\LoggerInterface;
 
 class Api {
@@ -417,36 +415,16 @@ class Api {
 
 	public function export ($request, $response, $args) {
 
-		// TODO: <b>Strict Standards</b>:  Declaration of Goodby\CSV\Export\Standard\CsvFileObject::fputcsv() should be compatible with SplFileObject::fputcsv($fields, $delimiter = NULL, $enclosure = NULL, $escape = NULL) in <b>/Volumes/DATA/Dev/umstudio/ums_redbean/api/vendor/goodby/csv/src/Goodby/CSV/Export/Standard/CsvFileObject.php</b> on line <b>84</b><br />
-
-		// init Goodby\CSV\Export\ 
-		if ( class_exists('Goodby\CSV\Export\Standard\ExporterConfig') ) {
-			$exportConfig = new ExporterConfig();
-
-			if ( class_exists('Goodby\CSV\Export\Standard\Exporter') ) {
-				$exporter = new Exporter($exportConfig);
-			}
-			else {
-				throw new Exception("CLASS NOT FOUND (Goodby\CSV\Export\Standard\Exporter)", 1);
-			}
-		}
-		else {
-			throw new Exception("CLASS NOT FOUND (Goodby\CSV\Export\Standard\ExporterConfig)", 1);
-		}
-
 		// collect data
 		$raw = R::findAll( $args['edge'] );
 
-		if ( !empty($raw) ) {
-
+		if (!empty($raw)) {
+			
 			// export it all
 			$data = R::exportAll( $raw, FALSE, array('NULL') );
 
-			// inject field keys to data as csv export table heading
-			$keys = array_keys($data[0]);
-			array_unshift($data, $keys);
-
-			// define outstream
+			// define csv properties
+			$headings = array_keys($data[0]);
 			$hashdate = str_replace(array(':','-',' '), '', R::isoDateTime());
 			$filename = 'export-'.$args['edge'].'-'.$hashdate.'.csv';
 
@@ -454,11 +432,22 @@ class Api {
 			header('Content-Type: text/csv');
 			header('Content-Disposition: attachment; filename='. $filename);
 
-			// TODO: how to export big tables? memory runs out.
-			$outstream = $exporter->export('php://output', $data);
+			$outstream = fopen('php://output', 'w');
+
+				// inject field keys to data as csv export table heading
+				fputcsv($outstream, $headings);
+
+				// foreach item, outstream fputcsv
+				foreach ($data as $row) {
+					fputcsv($outstream, $row);
+				}
+
+			// fclose
+			fclose($outstream);
 		}
 		else {
-			throw new Exception("Error Processing Request (edge raw data not found)", 1);
+			$err = array('error' => true, 'message' => getMessage('EDGE_NOTFOUND'));
+			return $response->withJson($err)->withStatus(404);			
 		}
 	}
 
